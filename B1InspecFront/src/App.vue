2c9ca6877a67b8225c03864c4a4d3caa6eab33fb
Davign10 <101421659+Davign10@users.noreply.github.com>
2023-09-12 19:47:41 -0300

    base front

---
+++ B1InspecFront/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import CadastroPrestador from './components/CadastroPrestador.vue'
+</script>
+
+<template>
+      <nav>
+        <RouterLink to="/cadPrestador"></RouterLink>
+      </nav>
+      <RouterView />
+</template>
+
+
