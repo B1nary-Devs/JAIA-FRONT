c86c7dbe8157042a4b6ba903467d51dfa94e6ef8
Davign10 <101421659+Davign10@users.noreply.github.com>
2023-09-12 22:43:39 -0300

    Esboçando componente tela de cadastro do prestador

--- B1InspecFront/src/App.vue
+++ B1InspecFront/src/App.vue
@@ -2,9 +2,11 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import CadastroPrestador from './components/CadastroPrestador.vue'
+import TheHeader from './components/The Header.vue';
 </script>
 
 <template>
+  <TheHeader></TheHeader>
       <nav>
         <RouterLink to="/cadPrestador"></RouterLink>
       </nav>
