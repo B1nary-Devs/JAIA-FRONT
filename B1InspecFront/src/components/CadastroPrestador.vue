3e7eb5864cb29e2de0f1c8a779babb9c1c33e455
Davign10 <101421659+Davign10@users.noreply.github.com>
2023-09-22 21:17:01 -0300

    #21 Finalização do post

--- B1InspecFront/src/components/CadastroPrestador.vue
+++ B1InspecFront/src/components/CadastroPrestador.vue
@@ -2,41 +2,40 @@
     <div class="form-cadastro">
         <div class="form-title">
             <h1>Prestador de Serviço</h1>
-            <span>> Cadastro</span>
+            <span> Cadastro</span>
         </div>
         <div class="form-body">
             <div class="input-group">
                 <div class="input-box">
                     <label for="id_nome">Nome</label>
-                    <input type="text" id="id_nome" placeholder="Ex: Encanador">
+                    <input type="text" id="id_nome" v-model="nome" placeholder="Ex: Encanador">
                 </div>
 
                 <div class="input-box">
                     <label for="id_cnpj">CNPJ</label>
-                    <input type="number" id="id_cnpj" placeholder="00.000.000/0000-00">
+                    <input type="number" id="id_cnpj" v-model="cnpj" placeholder="00.000.000/0000-00">
                 </div>
 
                 <div class="input-box">
-                    <label for="id_categoria">Categoria</label>
-                    <select id="id_categoria">
-                        <option v-for="ctg in categoria" name="select" :key="ctg.id" value={{ctg.id}}>{{ ctg.nome }}</option>
-                    </select>
+                  <label for="id_categoria">Segmento</label>
+                  <select id="id_categoria" v-model="categoriaSelecionada">
+                    <option v-for="ctg in categoria" :key="ctg.id" :value="ctg.id">{{ ctg.nome }}</option>
+                  </select>
                 </div>
 
                 <div class="input-box">
                     <label for="id_email">Email</label>
-                    <input type="email" id="id_email" placeholder="Ex: joao@gmail.com">
+                    <input type="email" id="id_email" v-model="email" placeholder="Ex: joao@gmail.com">
                 </div>
 
                 <div class="input-box">
                     <label for="id_senha">Senha</label>
-                    <input type="password" id="id_senha">
+                    <input v-model="senha" type="password" id="id_senha">
                 </div>
             </div>
 
             <div class="form-submit">
-                    <button>Voltar</button>
-
+                    <button class="button-return">Voltar</button>
                     <button @click="cadastrarPrestador">Cadastrar</button>
             </div>
 
@@ -49,16 +48,17 @@
 </template>
 
 <script setup lang="ts">
-  import { onMounted, ref } from "vue";
-  import '../assets/css/cadprestador/cadprestador.css'
+  import { onMounted, ref } from 'vue';
+  import '../assets/css/cadprestador/cadprestador.css';
   import axios from 'axios';
 
-  const nome = document.getElementById('id_nome')
-  const cnpj = ref("Cnpj");
-  const email = ref("Email");
-  const senha = ref("Senha");
+   // Capturando os valores dos campos
   const categoria = ref([]);
-  const prestador = ref();
+  const erro = ref();
+  const nome = ref("");
+  const cnpj = ref("");
+  const email = ref("");
+  const senha = ref("");
 
   async function coletarCategoria() {
     try {
@@ -70,28 +70,37 @@
     }
   }
 
-  async function cadastrarPrestador() {
-    await axios.post('http://localhost:8080/prestador' ,
-    {
+  const categoriaSelecionada = ref(null); // Inicialize com um valor padrão ou null
+
+async function cadastrarPrestador() {
+  // Verifique se uma categoria foi selecionada
+  if (categoriaSelecionada.value === null) {
+    alert('Selecione uma categoria antes de cadastrar.');
+    return;
+  }
+
+  // Fazendo a requisição POST com os valores capturados
+  try {
+    await axios.post('http://localhost:8080/prestador', {
       prestadorNome: nome.value,
       cnpj: cnpj.value,
       email: email.value,
       senha: senha.value,
-      categoria: categoria.value
-
+      categoria: {
+        id: categoriaSelecionada.value // Use a categoria selecionada aqui
+      }
     });
-    
-  }
 
-  function teste(){
-    const se = document.getElementById('id_categoria')
-    console.log(se.value);
-    
+    // Requisição bem-sucedida, exibir um alerta de confirmação
+    alert('Cadastro confirmado!');
+  } catch (error) {
+    console.error('Ocorreu um erro ao cadastrar o prestador:', error);
+    alert('Erro ao cadastrar o prestador.');
   }
+}
 
   onMounted(()=>{
     coletarCategoria();
   })
 
-  
 </script>
