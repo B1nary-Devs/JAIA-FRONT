841b66fcf50dd0d6cfc4ab3b55cdfbb0bd02d75a
Ludmila Chagas <81494654+ludmila-chagas@users.noreply.github.com>
2023-10-03 22:12:24 -0300

    72- Criação da tela de consulta

--- B1InspecFront/src/router/index.ts
+++ B1InspecFront/src/router/index.ts
@@ -40,6 +40,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ConsPrestador.vue')
+    },
+    {
+      path: '/consSegmento',
+      name: '/consSegmento',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ConsSegmento.vue')
     }
   ]
 })
