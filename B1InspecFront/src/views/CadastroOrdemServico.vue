a54179daaeab8378a9fdae355e0b43f5c7429de6
Davi Gusmão <davigusmao10@bol.com.br>
2023-10-15 22:51:37 -0300

    83 finish post new client

--- B1InspecFront/src/views/CadastroOrdemServico.vue
+++ B1InspecFront/src/views/CadastroOrdemServico.vue
@@ -8,11 +8,11 @@
 
       <div class="check-itens">
         
-        <input type="radio" value="existente" v-model="picked" />
-        <label for="">Selecionar cliente existente</label>
-        
-        <input type="radio" value="novo" v-model="picked" />
-        <label for="">Cadastrar novo cliente</label>
+        <input type="radio" value="existente" v-model="picked" @click="radio(true)"/>
+        <label for="existente">Selecionar cliente existente</label>
+
+        <input type="radio" value="novo" v-model="picked" @click="radio(false)"/>
+        <label for="novo">Cadastrar novo cliente</label>
         
       </div>
 
@@ -24,8 +24,8 @@
 
         <div class="input-box" v-else>
           <label for="id_selnome">Nome da Empresa</label>
-          <select id="id_selnome" v-model="nomeSelecionado">
-            <option v-for="ctg in cliente" :key="ctg.id" :value="ctg.id">{{ ctg.nome }}</option>
+          <select id="id_selnome" v-model="clienteSelecionado.value">
+          <option v-for="cliente in clienteSelecionado" :key="cliente.clienteId" :value="cliente.clienteId">{{ cliente.clienteNome }}</option>
           </select>
         </div>
 
@@ -74,27 +74,9 @@
       </div>
       <div class="itens" v-for="(checklistItem, index) in checklistsAtribuidos" :key="index">
         <div class="column">
-         <!-- {{ index + 1 }}: -->
+          {{ index + 1 }}:
           <span v-if="index !== estadoEdicao">{{ checklistItem.checklistNome }}</span>
           <input v-else v-model="checklistsAtribuidos[index].checklistNome" />
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" @click="editarItem(index)" v-if="index !== estadoEdicao" style="margin-right: 10px;">
-          <path fill="black" d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-          <path fill="black" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check2-square" viewBox="0 0 16 16" @click="salvarEdicao(index)" v-if="index === estadoEdicao" style="margin-right: 10px;">
-          <path fill="black" d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"/>
-          <path fill="black" d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" id="btn-remover" width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16" @click="removerItem(index)">
-          <path fill="black" d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-        </svg>
-      </div>
-      <div class="itens" v-for="(checklistPerson, index) in checklistsPersonalizado" :key="index">
-        <div class="column">
-          <!-- {{ index + 1 }}: -->
-          <span v-if="index !== estadoEdicao">{{ checklistPerson }}</span>
-          <input v-else v-model="checklistsPersonalizado[index]" />
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" @click="editarItem(index)" v-if="index !== estadoEdicao" style="margin-right: 10px;">
           <path fill="black" d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
@@ -113,7 +95,7 @@
 
       <div class="form-submit">
         <button @click="" class="button-return">Voltar</button>
-        <button @click="cadastrarOrdemServico">Cadastrar</button>
+        <button @click="cadastrarOrdemServico" >Cadastrar</button>
       </div>
     </div>
 
@@ -137,9 +119,10 @@ const cnpj = ref("");
 const descricao = ref("");
 const segmento = ref("");
 const checklist = ref("");
-const picked = ref('novo')
+const picked = ref('novo');
 const segmentoSelecionado = ref(null);
-const nomeSelecionado = ref(null);
+const clienteSelecionado = ref(null);
+const nomeSelecionado = ref([]);
 const prestador = ref(null);
 const prestadoresSelecionados = ref([]);
 const itens = ref<string[]>([])
@@ -147,17 +130,16 @@ const estadoEdicao = ref(-1)
 const checklistsAtribuidos = ref([]);
 const item = ref("");
 const itemEditando = ref(null);
-const checklistsPersonalizado = ref([]);
+const radioB = ref();
 
 function inserirItem() {
   console.log(`chechlistvalor -->  ${checklist.value}`)
   if (checklist.value.trim() !== "") {
-    checklistsPersonalizado.value.push(checklist.value);
-    console.log(`lista -->  ${checklistsPersonalizado.value}`)
+    checklistsAtribuidos.value.push(checklist.value);
+    console.log(`lista -->  ${checklistsAtribuidos.value}`)
     checklist.value = "";
   }
 }
-
 
 function editarItem(index) {
   estadoEdicao.value = index;
@@ -186,6 +168,14 @@ function removerItem(index) {
   checklistsAtribuidos.value.splice(index, 1);
 }
 
+
+function radio(valor: boolean){
+  radioB.value = valor;
+  console.log(clienteSelecionado.value);
+  const valor2 = clienteSelecionado.value;
+  console.log(`eu sou o valor ${valor2}`);
+}
+
 async function coletarSegmento() {
   try {
     const response = await axios.get('http://localhost:8080/segmento');
@@ -199,12 +189,14 @@ async function coletarSegmento() {
 async function coletarCliente() {
   try {
     const response = await axios.get('http://localhost:8080/cliente');
-    selnome.value = response.data; 
-    console.log(selnome.value);
+    const clientes = response.data;
+    clienteSelecionado.value = clientes; // Agora estamos preenchendo o valor selecionado
   } catch (error) {
     console.error('Ocorreu um erro ao coletar o segmento:', error);
   }
 }
+
+
 
 async function buscarChecklistsPorSegmento(segmentoId) {
   try {
@@ -284,10 +276,6 @@ async function cadastrarCliente() {
 }
 
 
-async function cadastrarChecklistPersonalizado() {
-
-}
-
 
 async function cadastrarOrdemServico() {
   if (!nome.value) {
@@ -300,11 +288,15 @@ async function cadastrarOrdemServico() {
       alert('Selecione um prestador de serviço');
       return;
     }
+    
+    //if (radioB.value){
+      //const clienteId = clienteSelecionado
+   
+    //}
 
     const clienteId = await cadastrarCliente();
 
   
-
     const ordemServicoData = {
       dataFechamento: null,
       status: "aberto em andamento",
