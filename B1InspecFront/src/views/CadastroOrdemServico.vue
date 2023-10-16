cad3ec9c876f3424069e85ad2c72447e2680e198
larissasouz <larissatomedesouza@gmail.com>
2023-10-15 21:07:58 -0300

    updatecheckperson

--- B1InspecFront/src/views/CadastroOrdemServico.vue
+++ B1InspecFront/src/views/CadastroOrdemServico.vue
@@ -74,9 +74,27 @@
       </div>
       <div class="itens" v-for="(checklistItem, index) in checklistsAtribuidos" :key="index">
         <div class="column">
-          {{ index + 1 }}:
+         <!-- {{ index + 1 }}: -->
           <span v-if="index !== estadoEdicao">{{ checklistItem.checklistNome }}</span>
           <input v-else v-model="checklistsAtribuidos[index].checklistNome" />
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" @click="editarItem(index)" v-if="index !== estadoEdicao" style="margin-right: 10px;">
+          <path fill="black" d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+          <path fill="black" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check2-square" viewBox="0 0 16 16" @click="salvarEdicao(index)" v-if="index === estadoEdicao" style="margin-right: 10px;">
+          <path fill="black" d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"/>
+          <path fill="black" d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" id="btn-remover" width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16" @click="removerItem(index)">
+          <path fill="black" d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+        </svg>
+      </div>
+      <div class="itens" v-for="(checklistPerson, index) in checklistsPersonalizado" :key="index">
+        <div class="column">
+          <!-- {{ index + 1 }}: -->
+          <span v-if="index !== estadoEdicao">{{ checklistPerson }}</span>
+          <input v-else v-model="checklistsPersonalizado[index]" />
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" @click="editarItem(index)" v-if="index !== estadoEdicao" style="margin-right: 10px;">
           <path fill="black" d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
@@ -129,12 +147,13 @@ const estadoEdicao = ref(-1)
 const checklistsAtribuidos = ref([]);
 const item = ref("");
 const itemEditando = ref(null);
+const checklistsPersonalizado = ref([]);
 
 function inserirItem() {
   console.log(`chechlistvalor -->  ${checklist.value}`)
   if (checklist.value.trim() !== "") {
-    checklistsAtribuidos.value.push(checklist.value);
-    console.log(`lista -->  ${checklistsAtribuidos.value}`)
+    checklistsPersonalizado.value.push(checklist.value);
+    console.log(`lista -->  ${checklistsPersonalizado.value}`)
     checklist.value = "";
   }
 }
