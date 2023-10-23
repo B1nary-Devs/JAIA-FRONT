30588caf40129513cba48a46c996b2691cfbbc09
julio cesar martins <juliocesar1316@gmail.com>
2023-10-22 22:45:55 -0300

    78/79 Montagem da tela e do css do login

---
+++ B1InspecFront/src/views/Login.vue
@@ -0,0 +1,60 @@
+
+<template>
+    <div class="loginPage">
+        <div class="login">
+            <h1>Login</h1>
+            <div class="input-box">
+                <label for="email">E-mail</label>
+                <input
+                    type="email"
+                    class="input-email"
+                    id="email"
+                    v-model="email"
+                />
+                <p v-if="errorEmail">E-mail Inválido!</p>
+            </div>
+            <div class="input-box">
+                <label for="senha">Senha</label>
+                <input
+                    type="password"
+                    class="input-senha"
+                    id="senha"
+                    v-model="senha"
+                />
+                <p v-if="errorSenha">Senha Inválida!</p>
+            </div>
+            <p>Esqueci a senha</p>
+            <div class="btnEntrar" @click="Login">
+                <button class="btn" >Entrar</button>
+            </div>
+        </div>
+        <ThePopUp/>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import '../assets/css/login/login.css'
+    import { ref } from 'vue'
+    import axios from 'axios'
+    import ThePopUp from '../components/ThePopUp.vue';
+    import {exibirPopup} from '../components/ThePopUp.vue'
+
+    const email = ref("")
+    const senha = ref("")
+    const errorEmail= ref(false)
+    const errorSenha = ref(false)
+
+    async function Login() {
+        try {
+            if(email.value === ""){
+                errorEmail.value = true
+            }
+            if(senha.value === ""){
+                errorSenha.value = true
+            }
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+</script>
