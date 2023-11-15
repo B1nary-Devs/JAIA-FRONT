ddef60809c586c55441d368ce73c37a32fab2d77
julio cesar martins <juliocesar1316@gmail.com>
2023-11-15 17:36:01 -0300

    130/131/132/126 Atualização de bugs e modal segmento

--- B1InspecFront/src/components/ModalSegmento.vue
+++ B1InspecFront/src/components/ModalSegmento.vue
@@ -25,9 +25,27 @@
                             Nome:
                         </p>
                         <span>{{ nomeSegmento }}</span>
-                        <div class="itens" v-for="checklist in checklistList" :key="checklist.checklistId">
-                                - {{ checklist.checklistNome }}
-                        </div>
+                        <!-- <div class="itens" v-for="x in checklist" :key="x.checklistId">
+                                - {{ x.checklistNome }}
+                        </div> -->
+                    </div>
+                    <div class="modal-box">
+                        <p>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-card-checklist"
+                                viewBox="0 0 16 16">
+                                <path
+                                    d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+                                <path
+                                    d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z" />
+                            </svg>
+                            Checklist:
+                        </p>
+                        <!-- <span>{{  xx.checklistPersonalizadoNome }}</span> -->
+                    </div>
+                    <div class="modal-box-check" v-for="x in checklist" :key="x.checklistId">
+                        <ul>
+                            <li><span>{{ x.checklistNome }}</span></li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -49,10 +67,11 @@ const props = defineProps({
     /* (1° PASSO) DECLARE AS PROPS DA ENTIDADE PARA PASSAR PARA OS CAMPOS HTML*/
     idSegmento: String,
     nomeSegmento: String,
-    checklistList: {
-        type: Array as PropType<{ checklistId: number, checklistNome: string }[]>, // Especifique o tipo
-        required: true,
-    }
+    // checklistList: {
+    //     type: Array as PropType<{ checklistId: number, checklistNome: string }[]>, // Especifique o tipo
+    //     required: true,
+    // }
+    checklist: Object
 });
 
 </script>
